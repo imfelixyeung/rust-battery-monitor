5374018078ffb82a5552092a1e7e203c8c604fb3
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2024-01-12 20:57:35 +0000

    add power

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,15 @@ struct BatteryInfoPayload {
     deviceSlug: String,
     batteryLevel: f32,
     interruptMode: String,
+    power: bool,
 }
 
-fn get_battery_level() -> Result<f32, battery::Error> {
+struct BatteryInfo {
+    battery_level: f32,
+    power: bool,
+}
+
+fn get_battery_info() -> Result<BatteryInfo, battery::Error> {
     let manager = battery::Manager::new();
 
     // handle errors
@@ -38,22 +44,32 @@ fn get_battery_level() -> Result<f32, battery::Error> {
         };
 
         let battery_level = battery.state_of_charge().value * 100.0;
-        return Ok(battery_level);
+        let power = battery.state() != battery::State::Discharging;
+
+        return Ok(BatteryInfo {
+            battery_level: battery_level,
+            power,
+        });
     }
 
-    return Ok(0.0);
+    return Ok(BatteryInfo {
+        battery_level: 0.0,
+        power: false,
+    });
 }
 
 fn main() {
     let api_endpoint = std::env::var("API_ENDPOINT").expect("API_ENDPOINT not set");
     let device_slug = std::env::var("DEVICE_SLUG").expect("DEVICE_SLUG not set");
 
-    let battery_level = get_battery_level().expect("Error getting battery level");
-    println!("Battery level: {}%", battery_level);
+    let info = get_battery_info().expect("Error getting battery level'");
+    println!("Battery level: {}%", info.battery_level);
+    println!("Power: {}", info.power);
 
     let payload = BatteryInfoPayload {
         deviceSlug: device_slug,
-        batteryLevel: battery_level,
+        batteryLevel: info.battery_level,
+        power: info.power,
         interruptMode: "none".to_string(),
     };
 
